0701ca5e56372890fd147530d9758ee12a5debd4
Shehan Kulathilake <shehanhere@gmail.com>
2023-02-07 23:44:53 +0530

    dynamodbclient->initialize dynamodb client upon construction

--- src/common/dao/dynamodb.crud.repo.ts
+++ src/common/dao/dynamodb.crud.repo.ts
@@ -1,7 +1,21 @@
 import { PageMeta, Page } from "../common.types";
 import { CRUDRepo } from "./crud.repo";
+import Client from '@aws-sdk/client-dynamodb';
+
 
 export class DynamodbCRUD<T,C,U> implements CRUDRepo<T,C,U>{
+    protected dynamodb: Client.DynamoDB;
+    
+    constructor(){
+        const config = {};
+        if(process.env.IS_OFFLINE) {
+            (config as any).region = "localhost";
+            (config as any).endpoint = "http://localhost:8000"
+        }
+        
+        this.dynamodb  = new Client.DynamoDB(config);
+    }
+
     create(create: C): Promise<T> {
         throw new Error("Method not implemented.");
     }
